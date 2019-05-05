81df80d750e6b16f1e51512bd7abc8c3eaa23f04
reon <425699861@qq.com>
2019-05-05 19:11:28 +0800

    DEBUG

--- InventorySystem/Items/ItemFactory.h
+++ InventorySystem/Items/ItemFactory.h
@@ -14,6 +14,10 @@ public:
 
 	virtual Item* Create(ItemType type, int id) = 0;
 
+	virtual bool IsItemMixed(string name) = 0;
+
+	virtual bool GetItemMixedList(string name) = 0;
+
 	// 道具合成表
 	vector<vector<string>> ItemMixTable;
 };
